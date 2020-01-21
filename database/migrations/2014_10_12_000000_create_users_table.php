5d36a159a188684798e034289f1d37df0c309275
cirill <cirill@asheart.fr>
2020-01-21 14:49:57 +0100

    migration / php artisan create file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('second_name');
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('civility')->nullable();
+            $table->string('facebook_api_token')->nullable();
         });
     }
 
