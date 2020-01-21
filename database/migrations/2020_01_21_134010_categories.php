03b0279eea0d773d5f157875487df7873d6cd90b
cirill <cirill@asheart.fr>
2020-01-21 15:09:47 +0100

    categories migration

--- database/migrations/2020_01_21_134010_categories.php
+++ database/migrations/2020_01_21_134010_categories.php
@@ -13,7 +13,10 @@ class Categories extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+        });
     }
 
     /**
