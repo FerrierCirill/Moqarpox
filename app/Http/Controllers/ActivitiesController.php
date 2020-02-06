857a9dd6405d334962067f224295edf8fed75ce8
Sylvian-Brunet <sylvian84@hotmail.fr>
2020-02-06 14:07:27 +0100

    Controller Activities - getOne - getAll - formAdd - formPost

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -2,23 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
+use App\Category;
+use App\SubCategory;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\AcceptHeaderItem;
 
 class ActivitiesController extends Controller
 {
     public function getActivities(){
-        //TODO
+        $activities = Activity::get();
+
+        return view('pages.activities', [
+            'activities' => $activities
+        ]);
     }
 
     public function getAddActivity(){
-        //TODO
+        $categories = Category::get();
+        $subCategories = SubCategory::get();
+
+        return view('pages.activity_add', [
+            'categories' => $categories,
+            'subCategories' => $subCategories
+        ]);
     }
 
-    public function postAddActivity(){
-        //TODO
+    public function postAddActivity(Request $request){
+
+        $this->validate ($request, [
+            'name' => 'required',
+            'price' => 'required|number',
+            'description' => 'required',
+            'resume' => 'required',
+            'information' => 'required',
+            'subCategory_id' => 'required|exists:subCategories,id',
+            'compagny_id' => 'required|exists:compagnies,id'
+        ]);
+
+        $activity = new Activity();
+        $activity->name = $request->input('name');
+        $activity->price = $request->input('price');
+        $activity->description = $request->input('description');
+        $activity->resume = $request->input('resume');
+        $activity->information = $request->input('information');
+        $activity->state = 'A vérifier';
+        $activity->note = 2.5;
+        $activity->subCategory_id = $request->input('subCategory_id');
+        $activity->compagny_id = $request->input('compagny_id');
+        $activity->save();
+
+        return redirect()->back();
     }
 
-    public function getActivity(){
-        //TODO
+    public function getActivity($activity_id){
+        $activity = Activity::findOrFail($activity_id);
+
+        return view('pages.activity_details', [
+            'activity' => $activity
+        ]);
     }
 }
