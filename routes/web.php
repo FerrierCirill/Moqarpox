48ec888a05c4d2fe37afda887486a60425e50914
BlackFox3000 <flo-ti@hotmail.fr>
2020-02-09 16:55:54 +0100

    Ajout de commentaire pour les routes. Edit du to do

--- routes/web.php
+++ routes/web.php
@@ -11,35 +11,41 @@
 |
 */
 
-Route::get('/', 'HomeController@test');
+Route::get('/', 'HomeController@test'); // Accueil
 Route::get('/random', 'HomeController@index');
 
-Route::get('/activities', 'ActivitiesController@getActivities')->name('activity_get_all');
-Route::get('/activity/add', 'ActivitiesController@getAddActivity')->name('activity_add_get');
-Route::post('/activity/add', 'ActivitiesController@postAddActivity')->name('activity_add_post');
-Route::get('/activity/{activity_id}', 'ActivitiesController@getActivity')->name('activity_details');
+Route::get('/activities', 'ActivitiesController@getActivities')->name('activity_get_all');           // liste toutes les activitées
+Route::get('/activity/add', 'ActivitiesController@getAddActivity')->name('activity_add_get');        // Add Act
+Route::post('/activity/add', 'ActivitiesController@postAddActivity')->name('activity_add_post');     // Add Act
+Route::get('/activity/{activity_id}', 'ActivitiesController@getActivity')->name('activity_details'); // Détail Act
 
-Route::get('/compagnies', 'CompagniesController@getCompagnies')->name('compagny_get_all');
-Route::get('/compagny/add', 'CompagniesController@getAddCompagny')->name('compagny_add_get');
-Route::post('/compagny/add', 'CompagniesController@postAddCompagny')->name('compagny_add_post');
-Route::get('/compagny/{compagny_id}', 'CompagniesController@getCompagny')->name('compagny_details');
-Route::get('/compagny/{compagny_id}/edit', 'CompagniesController@editCompagny')->name('compagny_edit');
+Route::get('/compagnies', 'CompagniesController@getCompagnies')->name('compagny_get_all');              // Liste les compagny
+Route::get('/compagny/add', 'CompagniesController@getAddCompagny')->name('compagny_add_get');           // Add comp
+Route::post('/compagny/add', 'CompagniesController@postAddCompagny')->name('compagny_add_post');        // Add comp
+Route::get('/compagny/{compagny_id}', 'CompagniesController@getCompagny')->name('compagny_details');    // Mon entreprise/struct
+Route::get('/compagny/{compagny_id}/edit', 'CompagniesController@editCompagny')->name('compagny_edit'); // Edit comp
 
-Route::get('/customer/{user_id}', 'UsersController@getCustomer')->name('customer_details');
-Route::get('/customer/{user_id}/historical', 'UsersController@historical')->name('customer_historical');
-Route::get('/customer/{user_id}/shoppingCart', 'HomeController@shoppingCart')->name('customer_shopping_cart');
-Route::get('/provider/{user_id}', 'UsersController@getProvider')->name('provider_details');
+Route::get('/customer/{user_id}', 'UsersController@getCustomer')->name('customer_details');                   // Profil
+Route::get('/customer/{user_id}/historical', 'UsersController@historical')->name('customer_historical');      // Historique
+Route::get('/customer/{user_id}/shoppingCart', 'HomeController@shoppingCart')->name('customer_shopping_cart');// Panier
+Route::get('/provider/{user_id}', 'UsersController@getProvider')->name('provider_details');                   // Mes entreprises/struct
 
-Route::get('/legalMentions', 'HomeController@LM')->name('legal_mentions');
-Route::get('/termsConditionsUse', 'HomeController@TCU')->name('terms_conditions_use');
-Route::get('/termsConditionsSale', 'HomeController@TCS')->name('terms_conditions_sale');
+Route::get('/legalMentions', 'HomeController@LM')->name('legal_mentions');                 //Mentions légal
+Route::get('/termsConditionsUse', 'HomeController@TCU')->name('terms_conditions_use');     // CGU
+Route::get('/termsConditionsSale', 'HomeController@TCS')->name('terms_conditions_sale');   // CGV
 
 Route::get('/design', 'HomeController@index'); // a edit quand ca sera bon pour la route '/'
 
+Route::get('/admin','AdminController@index'); // a edit
 /*
  * TODO
- * Paiement / Remboursement
+ * Paiement / Remboursement(optionel)
  * Rembourser un bon-cadeau
  * Tout le systeme d'authentification (floren est dessus)
  * Modérateur / Admin
+ * Déposer un avis
+ * Gestion commentaire
+ * Gestion Activités
+ * Gestion Structures
+ * Gestions ( admin panel)
  */
