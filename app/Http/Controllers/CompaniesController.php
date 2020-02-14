6677478f03158f8278f079fd3606e968c5e80372
BlackFox3000 <flo-ti@hotmail.fr>
2020-02-14 11:36:59 +0100

    Supression de la table picture, Ajout de link à Compagnies Ajout de link1,2,3 à Activities Création addCompagny

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Company;
 use App\City;
+use App\Picture;
 use Illuminate\Http\Request;
 
 class CompaniesController extends Controller
@@ -16,8 +17,44 @@ class CompaniesController extends Controller
         ]);
     }
 
-    public function postAddCompany() {
-        $companny = new Company;
+    public function postAddCompany(Request $request) {
+        $this->validate ($request, [
+            'name' => 'required',
+            'phone' => 'required|number',
+            'email' => 'required',
+            'siret' => 'required',
+            'rib' => 'required',
+            'adress1' => 'required',
+            'adress2' => 'required',
+            'lat' => 'required',
+            'lng' => 'required',
+            'city_id' => 'required',
+            'description' => 'required',
+            'category_id' => 'required|exists:categories,id',
+            'user_id' => 'required',
+        ]);
+
+
+        $compagny = new Company();
+        $compagny->name = $request->input('name');
+        $compagny->phone = $request->input('phone');
+        $compagny->email = $request->input('email');
+        $compagny->siret = $request->input('siret');
+        $compagny->rib = $request->input('rib');
+        $compagny->adress1 = $request->input('adress1');
+        $compagny->adress2 = $request->input('adress2');
+        $compagny->city_id = $request->input('city_id');
+        $compagny->description = $request->input('description');
+        $compagny->category_id = $request->input('category_id');
+        $compagny->user_id = $request->input('user_id');
+        $compagny->lat = $request->input('lat');
+        $compagny->lng = $request->input('lng');
+        $compagny->save();
+
+        //Voir pour le cas ou l'on envoie une image
+
+
+        return redirect()->back();
 
     }
 
