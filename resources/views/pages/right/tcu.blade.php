f352de364bd37574789a47ea7559f7124c1c5dfc
unknown <MIW@A-F004345.salsa.univ-amu.fr>
2020-02-28 10:18:11 +0100

    Début des CGU

---
+++ resources/views/pages/right/tcu.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+  <h2> Conditions Générales d’Utilisations - CGU Mouqarpox - mouqarpox.miw.ovh </h2>
+
+
+
+  <div class="row">
+<div class="offset-s1 col"> Mise à jour le 27 Janvier 2019 </div></div></br>
+
+  <h3> 1. Condtions Générales d'Utilisations Clients </h3>
+
+  <h4> a. Préambule </h4>
+
+  <br/>
+  <div class="row">
+    <div class="offset-s1 col">
+
+    <strong> Mouqarpox </strong> est un <strong> Service </strong> proposé par la société <strong> Mouqarpox </strong>. <br/>
+    <strong> Mouqarpox </strong> met en place sur mouqarpox.miw.ovh (<strong> Site </strong>) un <strong> Service </strong> permettant à des clients, après inscription, d'acheter des <strong> Bons-cadeau </strong> proposés par des <strong> Prestataires </strong>, à un prix ferme. <br/>
+    Pour utiliser le <strong> Service </strong> , le <strong> Client </strong> doit accepter les Conditions Générales d’Utilisations Clients (CGU <strong> Clients </strong>) sans restriction ni réserve. Cela sera matérialisé par un clic de validation lors de l’inscription. <br/>
+    Les ventes réalisées par l’intermédiaire du <strong> Service </strong> sont quant à elles régies par les Conditions Générales de Ventes (CGV) qui doivent être également acceptées par le <strong> Client </strong> lors de chaque achat. <br/>
+
+    </div>
+  </div></br>
+
+  <h4> b. Objet </h4>
+
+  <br/>
+  <div class="row">
+<div class="offset-s1 col">
+
+  Les <strong> CGU </strong> <strong> Clients </strong> ont pour objet de définir les conditions dans lesquelles <strong> Mouqarpox </strong> met à la disposition des <strong> Clients </strong>, dans le cadre du <strong> Service </strong> , des outils technologiques utilisés pour acheter des <strong> Bons-Cadeau </strong> auprès des <strong> Prestataires </strong>. <br/>
+
+  </div></div></br>
+
+  <h4> c. Description de service </h4>
+
+  <br/>
+  <div class="row">
+<div class="offset-s1 col">
+
+  Le <strong> Service </strong> est constitué d'un ensemble d'outils permettant aux <strong> Clients </strong> de s'inscrire sur le <strong> Site </strong>, de passer des commandes de <strong> Bons-Cadeau </strong>, de régler le prix des <strong> Bons-Cadeau </strong>, de recevoir par e-mail la confirmation de l’achat. <br/>
+  Il intègre également un système de commentaires permettant aux <strong> Clients </strong> d'évaluer la qualité d’un <strong> Bon-Cadeau </strong>. <br/>
+  Chaque <strong> Client </strong> s'engage à conclure les achats en toute bonne foi. <br/>
+
+  </div></div></br>
+
+  <h4> d. Accès au service </h4>
+
+  <br/>
+  <div class="row">
+<div class="offset-s1 col">
+
+  L'accès au <strong> Service </strong> par les <strong> Clients </strong> est réservé à une utilisation strictement personnelle. Lorsqu'il utilise le <strong> Service </strong> , le <strong> Client </strong> déclare agir à titre privé. <br/>
+  L’accès au <strong> Service </strong> est totalement libre, mais afin de procéder au règlement d’une commande,  vous devrez créer un compte. <br/>
+  Lors de la création de ce compte, vous vous engagez à ne fournir que des informations exactes. <br/>
+  Pour utiliser le <strong> Service </strong> , le <strong> Client </strong> doit utiliser l'identifiant et le mot de passe créés lors de l'ouverture de son compte. Le <strong> Client </strong> s’engage à les conserver secrets et à ne les divulguer à aucun tiers. <br/>
+  En cas de perte, de détournement ou d'utilisation frauduleuse de votre identifiant et/ou de votre mot de passe, vous vous engagez à en avertir immédiatement <strong> Mouqarpox </strong>. <br/>
+
+  </div></div></br>
+
+  <h4> e. Prix du service </h4>
+
+  <br/>
+  <div class="row">
+<div class="offset-s1 col">
+
+  L'ouverture d'un compte et l'utilisation du <strong> Service </strong> sont gratuites (hors coûts éventuels de la connexion, dont le prix dépend de votre opérateur de communication électronique), sans obligation d'achat sur le <strong> Site </strong>. Seul l'achat de <strong> Bons-cadeau </strong> à des <strong> Prestataires </strong> est payant, dans les CGV.<br/>
+
+  </div></div></br>
+
+  <h4> f. Données personnelles </h4>
+
+  <br/>
+  <div class="row">
+<div class="offset-s1 col">
+
+  Les informations et données <strong> Vous </strong> concernant sont traitées par <strong> Mouqarpox </strong>, elles sont nécessaires à la gestion de votre compte. <br/>
+
+  </div></div></br>
+
+  <h4> g. Propriété intellectuelle </h4>
+
+  g.1 - Tous les textes, commentaires, ouvrages, illustrations, œuvres et images reproduits ou représentés sur le <strong> Site </strong> sont strictement réservés au titre du droit d'auteur ainsi qu'au titre de la propriété intellectuelle, pour toute la durée de protection de ces droits et pour le monde entier. À ce titre et conformément aux dispositions du Code de la propriété intellectuelle, seule l'utilisation pour un usage privé, sous réserve de dispositions différentes voire plus restrictives du code de la propriété intellectuelle, est autorisée. Toute reproduction, représentation, modification ou adaptation totale ou partielle du <strong> Site </strong> et/ou de tout ou partie des éléments se trouvant sur le <strong> Site </strong> ou qui y sont incorporés est strictement interdite.<br/>
+
+  g.2 - Les dénominations sociales, marques et signes distinctifs reproduits sur le <strong> Site </strong> sont protégés au titre du droit des marques. La reproduction ou la représentation de tout ou partie d'un des signes précités est strictement interdite et doit faire l'objet d'une autorisation écrite préalable du titulaire de la marque.<br/>
+
+  g.3 - Certains <strong> Bons-Cadeau </strong> font l'objet de droits d'utilisation personnels et spécifiques réglementant les copies, diffusions publiques, locations etc. Vous devez respecter les conditions contractuelles applicables à ces <strong> Bons-Cadeau </strong> et <strong> Mouqarpox </strong> ne saurait être responsable des utilisations qui pourraient être faites de ces <strong> Bons-Cadeau </strong> dans ce cadre.<br/>
+
+  <h4> h. Responsabilité </h4>
+
+  La responsabilité de <strong> Mouqarpox </strong> envers le <strong> Client </strong> ne peut être engagée que pour des faits qui lui seraient directement imputables et qui lui causeraient un préjudice directement lié à ces faits. Elle ne peut être engagée pour les préjudices indirects. La responsabilité de Mouqarpox ne peut non plus être engagée du fait de la mauvaise utilisation du <strong> Service </strong> par <strong> l </strong>'Acheteur ou de toute faute de sa part. Elle ne saurait pas plus être engagée à raison de faits imputables à un tiers au <strong> Service </strong>.<br/>
+  En outre, conformément à l'article 6-I, 2 et 3 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, <strong> Mouqarpox </strong> ne saurait voir sa responsabilité engagée à raison des contenus rendus disponibles sur le <strong> Site </strong> par les <strong> Clients </strong> ou les <strong> Prestataires </strong>, notamment en ce qui concerne les appréciations que les <strong> Clients </strong> font des <strong> Bons-Cadeau </strong>, sauf si elle ne les rendait pas promptement inaccessibles après avoir été informée de leur illicéité dans les conditions prévues par cette loi. <br/>
+  Le <strong> Client </strong> est seul responsable de l'utilisation qu'il fait du <strong> Service </strong>, notamment des appréciations qu'il fait sur le <strong> Site </strong>, et s'engage à garantir à première demande à indemniser et dédommager Mouqarpox de tout dommage, perte, manque à gagner, que <strong> Mouqarpox </strong> pourrait subir si sa responsabilité se trouvait engagée par un tiers, du fait d'une action liée à cette utilisation du <strong> Service </strong> par le <strong> Client </strong>.<br/>
+
+  <h4> i. Nullité partielle </h4>
+
+  Si une ou plusieurs stipulations des présentes <strong> CGU Clients </strong> sont tenues pour non valides ou déclarées comme tel en application d'une loi, d'un règlement ou d'une décision définitive d'une juridiction compétente, les autres stipulations garderont toute leur force et toute leur portée.<br/>
+
+  <h4> j. Cookies tiers </h4>
+
+  Lorsque vous naviguez sur le site <strong> Internet </strong> de la société <strong> Mouqarpox </strong>, nous pouvons être amenés à installer des cookies variés sur le disque dur de votre ordinateur ou de votre appareil mobile. Les cookies utilisés par le site internet <strong> Mouqarpox </strong> le sont pour les finalités décrites ci-dessous.<br/>
+  Les cookies de fonctionnement. Lorsque vous naviguez sur le site internet de la société <strong> Mouqarpox </strong>, nous pouvons être amenés à installer des cookies qui sont nécessaires au fonctionnement de nos sites internet. Il s’agit par exemple, des cookies qui vous permettent d’accéder aux zones sécurisées de nos sites. Nous vous informons que ces cookies ne collectent pas d’information pouvant être utilisées à des fins de marketing ou permettant de garder en mémoire votre historique de navigation sur internet. Cette catégorie de cookies ne peut pas être désactivée.<br/><br/>
+
+  <strong> ITALIC Les cookies émis sur nos sites par nos partenaires. ITALIDC </strong> <br/><br/>
+
+  Nos sites internet utilisent des cookies Google Analytics. Les informations collectées par les cookies Google Analytics seront transmises et hébergées par Google sur des serveurs situés aux Etats-Unis d’Amérique conformément à leur politique de confidentialité. Pour une présentation de la politique de confidentialité de Google et savoir comment cela s’applique à Google Analytics, visitez <a href="http://www.google.com/intl/fr/policies/">http://www.google.com/intl/fr/policies/</a>.<br/><br/>
+
+  Nos sites internet utilisent des cookies Mapbox Service de cartographie. Les informations collectées par les cookies Mapbox seront transmises et hébergées par Mapbox sur des serveurs situés aux Etats-Unis d’Amérique conformément à leur politique de confidentialité. Pour une présentation de la politique de confidentialité de Mapbox et savoir comment cela s’applique, visitez <a href="https://www.mapbox.com/">https://www.mapbox.com/</a><br/>
+
+  <h3> 2. Conditions Générales d’Utilisations Prestataires </h3>
+
+  <h4> a. Préambule </h4>
+
+  <strong> Mouqarpox </strong> met en place, sur http://moqarpox.miw.ovh (<strong> Site </strong>), une Place de Marché permettant à des <strong> Prestataires </strong> majeurs (<strong> Vendeurs </strong> ou <strong> Vous </strong>), après inscription, de rentrer, par son intermédiaire, en relation avec des acheteurs particuliers (<strong> Acheteurs </strong>), également inscrits sur le <strong> Site </strong>, dans le but de vendre des <strong> Bons-cadeau </strong> (<strong> Bon-cadeau </strong>) à prix fermes.<br/><br/>
+
+  Pour utiliser le <strong> Service </strong>, le <strong> Prestataire </strong> accepte de se soumettre aux présentes Conditions Générales d'Utilisation <strong> Prestataire </strong> (CGU ou <strong> Prestataire </strong>) sans restriction ni réserve. Cette acceptation se matérialise par un clic de validation lors de son inscription sur le <strong> Service </strong>.<br/><br/>
+
+  Les ventes réalisées par l'intermédiaire du <strong> Service </strong> entre les <strong> Acheteurs </strong> et les <strong> Prestataires </strong> sont quant à elles régies par les Conditions Générales de Vente (CGV) qui sont réputées acceptées par le <strong> Prestataire </strong> lors de chaque confirmation de commande, elles complètent les présentes CGU <strong> Prestataire </strong>.<br/><br/>
+
+  <strong> Mouqarpox </strong> <strong> Vous </strong> invite à prendre connaissance des CGV en cliquant sur les Conditions Générales d'Utilisation <strong> Prestataires </strong>. <strong> Mouqarpox </strong> se réserve la possibilité de modifier les présentes CGU.<br/><br/>
+
+  Toute modification prendra effet immédiatement pour les <strong> Bons-Cadeau </strong> en ligne dès leur notification au <strong> Prestataire </strong>. Elles ne s'appliqueront pas pour les transactions en cours au moment de leur entrée en vigueur.<br/><br/>
+
+  <h4> b. Objet </h4>
+
+  Les présentes CGU <strong> Prestataire </strong> ont pour objet de définir les conditions dans lesquelles <strong> Mouqarpox </strong> met à la disposition des <strong> Prestataires </strong>, dans le cadre du Service, des outils technologiques permettant à ceux-ci de mettre en vente les <strong> Bons-Cadeau </strong>.<br/>
+
+  <h4> c. Description du service </h4>
+
+  Le <strong> Service </strong> est constitué d'un ensemble d'outils permettant aux <strong> Prestataires </strong> de se mettre en relation avec des Acheteurs, afin de leur proposer des <strong> Bons-Cadeau </strong> à la vente, de référencer et de décrire ces <strong> Bons-Cadeau </strong> , d'accepter les commandes passées par les Acheteurs, de percevoir le prix des <strong> Bons-Cadeau </strong> achetés et de gérer le <strong> Service </strong> après-vente pour les <strong> Bons-Cadeau </strong> vendus.<br/><br/>
+
+  Les transactions effectuées via le <strong> Service </strong> pour les besoins de la vente des <strong> Bons-Cadeau </strong> sont conclues directement entre <strong> l'Acheteur </strong> et le <strong> Prestataire </strong> . <strong> Mouqarpox </strong> n'est en aucun cas revendeur des <strong> Bons-Cadeau </strong> proposés par les <strong> Prestataires </strong> par l'intermédiaire du <strong> Service </strong>. Par ailleurs, en cas de non-respect des présentes CGU <strong> Prestataire </strong>, <strong> Mouqarpox </strong> se réserve le droit d'interrompre de manière temporaire ou définitive l'accès au <strong> Service </strong> au <strong> Prestataire </strong> concerné, en suspendant ou en résiliant les présentes dans les conditions prévues à l’article 7.<br/>
+
+  <h4> d. Accès au service </h4>
+
+  Outre l'acceptation des présentes CGU <strong> Prestataire </strong>, l'accès au <strong> Service </strong> est subordonné à l'ouverture d'un compte <strong> Mouqarpox </strong>. Vous devez pour cela fournir les données permettant votre identification ainsi que vos coordonnées. Lors de l'ouverture de ce compte, <strong> Vous </strong> vous engagez à ne fournir que des informations exactes, puis à informer <strong> Mouqarpox </strong> sans délai de tout changement les affectant.
+</div>
+
+@endsection
