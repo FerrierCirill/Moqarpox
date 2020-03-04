db6fd5830edcfa823d874fdff29d03a12ab9965b
cirill <cirill@asheart.fr>
2020-03-04 14:34:31 +0100

    footer

--- resources/views/layouts/utils/footer.blade.php
+++ resources/views/layouts/utils/footer.blade.php
@@ -1,19 +1,30 @@
 <footer class="page-footer">
     <div class="container">
-        @php
-            $footer_category = App\Category::all()
-        @endphp
         <div class="row">
-            @foreach ($footer_category as $category)
-                <div class="col s12 m6 l3">
-                    <h5 class="white-text">{{$category->name}}</h5>
-                    <ul>
-                        @foreach($category->subcategories as $subcategory)
-                            <li><a class="grey-text text-lighten-3" href="#!">{{$subcategory->name}}</a></li>
-                        @endforeach
-                    </ul> 
-                </div>
-            @endforeach
+            <div class="col s12 m6 l4">
+                <h5>A propos de Mouqarpox</h5>
+                    <a href="{{route('legal_mentions')}}" class="white-text">Qui sommes-nous ?</a>
+                <br><a href="#!" class="white-text">Charte qualité</a>
+                <br><a href="#!" class="white-text">Idées séjours Mouqar-Weekend</a>
+                <br><a href="#!" class="white-text">Recrutement</a>
+                <br><a href="#!" class="white-text">Partenaria Mouqarpox</a>
+            </div>
+
+            <div class="col s12 m6 l4">
+                <h5>Pour les professionnels</h5>
+                    <a href="{{route('company_add_get')}}" class="white-text">Devenir partenaire ou parrainer un établissement</a>
+                <br><a href="{{route('user_details')}}"    class="white-text">Accéder à votre Espace Partenaire</a>
+                <br><a href="#!" class="white-text">Solutions pour entreprises</a>
+                <br><a href="#!" class="white-text">Solutions pour comités d'entreprise</a>
+            </div>
+
+            <div class="col s12 m6 l4">
+                <h5>Besoin d'aide ?</h5>
+                    <a href="#!" class="white-text">Aide</a>
+                <br><a href="#!" class="white-text">Comment ça marche ?</a>
+                <br><a href="{{route('terms_conditions_sale')}}" class="white-text">Conditions de livraison</a>
+                <br><a href="{{route('terms_conditions_sale')}}" class="white-text">Échange et prolongation</a>
+            </div>
         </div>
     </div>
     <div class="footer-copyright">
