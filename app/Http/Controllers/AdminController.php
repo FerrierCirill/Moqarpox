3500242f2e46a662580d2e4a83609df78e2cb73b
cirill <cirill@asheart.fr>
2020-03-12 09:06:05 +0100

    Design de admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Activity;
 use App\Comment;
 use App\Company;
 use App\User;
+use App\ActivityOrder;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -16,27 +17,58 @@ class AdminController extends Controller
         $companies  = Company::where('state', 0)->paginate(5);
         $comments   = Comment::where('state', 0)->paginate(5);
 
-        $numberOfCompanies = Company::get()->count();
+        $numberOfCompanies  = Company::get()->count();
         $numberOfActivities = Activity::get()->count();
 
-        $numberOfCompaniesValidate = Company::where('state', 1)->get()->count();
+        $numberOfCompaniesValidate  = Company::where('state', 1)->get()->count();
         $numberOfActivitiesValidate = Activity::where('state', 1)->get()->count();
 
         $nombre_activities_attente = Activity::where('state', 0)->get()->count();
-        $nombre_companies_attente = Company::where('state', 0)->get()->count();
-        $nombre_comments_attente = Comment::where('state', 0)->get()->count();
+        $nombre_companies_attente  = Company::where('state', 0)->get()->count();
+        $nombre_comments_attente   = Comment::where('state', 0)->get()->count();
+
+        $orders = ActivityOrder::get()->count();
+
+        $NowYear = date('Y');
+        $NowMouth = date('m');
+
+        $nombre_companies_on_last_12_mouth  = [];
+        $nombre_activities_on_last_12_mouth = [];
+        $nombre_orders_on_last_12_mouth     = [];
+
+        for ($i=0; $i < 12; $i++) { 
+            $nombre_companies_on_last_12_mouth[]  = Company:: whereMonth('created_at', $NowMouth)
+                                                            ->whereYear ('created_at', $NowYear)->get()->count();
+            $nombre_activities_on_last_12_mouth[] = Activity::whereMonth('created_at', $NowMouth)
+                                                            ->whereYear ('created_at', $NowYear)->get()->count();
+            $nombre_orders_on_last_12_mouth[]= ActivityOrder::whereMonth('created_at', $NowMouth)
+                                                            ->whereYear ('created_at', $NowYear)->get()->count();
+
+
+            if($NowMouth == 1) {
+                $NowMouth = 12;
+                $NowYear--;
+            }
+            else {
+                $NowMouth--;
+            }
+        }
 
         return view('pages.admin.moderation', [
-            'activities' => $activities,
-            'nombre_activities_attente' => $nombre_activities_attente,
-            'companies' => $companies,
-            'nombre_companies_attente' => $nombre_companies_attente,
-            'comments' => $comments,
-            'nombre_comments_attente' => $nombre_comments_attente,
-            'nombre_companies' => $numberOfCompanies,
-            'nombre_activities' => $numberOfActivities,
-            'nombre_companies_valide' => $numberOfCompaniesValidate,
-            'nombre_activities_valide' => $numberOfActivitiesValidate
+            'activities'                         => $activities,
+            'nombre_activities_attente'          => $nombre_activities_attente,
+            'companies'                          => $companies,
+            'nombre_companies_attente'           => $nombre_companies_attente,
+            'comments'                           => $comments,
+            'orders'                             => $orders,
+            'nombre_comments_attente'            => $nombre_comments_attente,
+            'nombre_companies'                   => $numberOfCompanies,
+            'nombre_activities'                  => $numberOfActivities,
+            'nombre_companies_valide'            => $numberOfCompaniesValidate,
+            'nombre_activities_valide'           => $numberOfActivitiesValidate,
+            'nombre_companies_on_last_12_mouth'  => $nombre_companies_on_last_12_mouth,
+            'nombre_activities_on_last_12_mouth' => $nombre_activities_on_last_12_mouth,
+            'nombre_orders_on_last_12_mouth'     => $nombre_orders_on_last_12_mouth
         ]);
     }
 
--- resources/views/pages/admin/moderation.blade.php
+++ resources/views/pages/admin/moderation.blade.php
@@ -9,137 +9,238 @@
 @section('content')
 
 <div class="container pt-2">
-  <h1> Panel Administration</h1>
+	<h1> Panel Administration</h1>
     <div class="row">
-        <div class="col s12 z-depth-1 px-1">
+        <div class="col s12 z-depth-1 px-1 mb-3">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"></script>
             <h5>Statistiques</h5>
             <div>
-                <div class="col s6 z-depth-1 mb-1">
-                    <div class="col s12 pb-1">
-                        <div class="col s8 pt-1">
+                <div class="col s12 mb-1">
+                    <div class="col m4 s12 pb-1">
+                        <div class="col m8 s12 pt-1">
                             Nombres d'entreprises créer : <br>
                             Nombres d'entreprises validé : <br>
                             Nombres d'entreprises en attente : <br>
                         </div>
-                        <div class="col s4 right-align pt-1">
+                        <div class="col m4 s12 right-align pt-1">
                             {{ $nombre_companies }}<br>
                             {{ $nombre_companies_valide }} <br>
                             {{ $nombre_companies_attente }} <br>
                         </div>
                     </div>
-                    <div class="col s12 pb-1">
-                        <div class="col s8">
+
+                    <div class="col m8 s12 pb-1">
+						<canvas id="companyChar"  height="100" style='color:#fff'></canvas>
+
+                        <script>
+							let dataCompanyChar  = JSON.parse('{{json_encode($nombre_companies_on_last_12_mouth)}}')
+							let ctxCompanyChar   = document.getElementById('companyChar').getContext('2d');
+							let chartCompanyChar = new Chart(ctxCompanyChar, {
+								type: 'bar',
+								data: {
+									labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
+									datasets: [{
+										label          : 'Entreprises créer',
+										backgroundColor: '#aed581',
+										borderColor    : 'rgb(255, 255, 255)',
+										fillColor      : "#aed581",
+										data           : dataCompanyChar
+									}]
+								},
+								options: {
+									responsive: true,
+									defaultFontColor : '#fff',
+									labelColor: "#fff",
+									legend: {
+										font: {
+											color: "#fff"
+										}
+									},
+									scaleFontColor: "#fff",
+								}
+							});
+                        </script>
+                    </div>
+				</div>
+				
+                <div class="col s12 mb-1">
+					<div class="col m4 s12 pb-1">
+                        <div class="col m8 s12 pt-1">
                             Nombres d'activités créer : <br>
                             Nombres d'activités validé : <br>
                             Nombres d'activités en attente : <br>
                         </div>
-                        <div class="col s4 right-align">
+                        <div class="col m4 s12 right-align pt-1">
                             {{ $nombre_activities }} <br>
                             {{ $nombre_activities_valide }} <br>
                             {{ $nombre_activities_attente }} <br>
                         </div>
                     </div>
-                    <div class="col s12 pb-1">
-                        <div class="col s8">
+
+                    <div class="col m8 s12 pb-1">
+						<canvas id="activityChar" height="100" style='color:#fff'></canvas>
+
+                        <script>
+							let dataActivityChar  = JSON.parse('{{json_encode($nombre_activities_on_last_12_mouth)}}')
+							let ctxActivityChar   = document.getElementById('activityChar').getContext('2d');
+							let chartActivityChar = new Chart(ctxActivityChar, {
+								type: 'bar',
+								data: {
+									labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
+									datasets: [{
+										label          : 'Activités créer',
+										backgroundColor: '#aed581',
+										borderColor    : 'rgb(255, 255, 255)',
+										fillColor      : "#aed581",
+										data           : dataActivityChar
+									}]
+								},
+								options: {
+									responsive: true,
+									defaultFontColor : '#fff',
+									labelColor: "#fff",
+									legend: {
+										font: {
+											color: "#fff"
+										}
+									},
+									scaleFontColor: "#fff",
+								}
+							});
+                        </script>
+					</div>
+				</div>
+
+				<div class="col s12 mb-1">
+					<div class="col m4 s12 pb-1">
+                        <div class="col m8 s12 pt-1">
                             Nombres de commandes réalisé : <br>
                             Moyenne de commandes par activité : <br>
                         </div>
-                        <div class="col s4 right-align">
-                            TODO <br>
+                        <div class="col m4 s12 right-align pt-1">
+                            {{ $orders }} <br>
                             TODO <br>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col s6 z-depth-1 mb-1">
-                <div class="col s12 pb-1 pt-1">
-                    TODO
-                </div>
+
+                    <div class="col m8 s12 pb-1">
+						<canvas id="orderChar" height="100" style='color:#fff'></canvas>
+
+                        <script>
+							let dataOrderChar  = JSON.parse('{{json_encode($nombre_orders_on_last_12_mouth)}}')
+							let ctxOrderChar   = document.getElementById('orderChar').getContext('2d');
+							let chartOrderChar = new Chart(ctxOrderChar, {
+								type: 'bar',
+								data: {
+									labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
+									datasets: [{
+										label          : 'Nombres de commandes',
+										backgroundColor: '#aed581',
+										borderColor    : 'rgb(255, 255, 255)',
+										fillColor      : "#aed581",
+										data           : dataOrderChar
+									}]
+								},
+								options: {
+									responsive: true,
+									defaultFontColor : '#fff',
+									labelColor: "#fff",
+									legend: {
+										font: {
+											color: "#fff"
+										}
+									},
+									scaleFontColor: "#fff",
+								}
+							});
+                        </script>
+					</div>
+				</div>
             </div>
         </div>
     </div>
-  <ul class="collapsible accordeon">
-    <li>
-      <div class="collapsible-header">
-        Structures en attente ({{ $nombre_companies_attente }})
-      </div>
-      <div class="collapsible-body">
-        <h5 class="pb-2">Structures en attente</h5>
-        <ul>
-            @foreach($companies as $company)
-                <li>
-                    <div class="row z-depth-1 pt-1 px-1 pb-1">
-                        <div class="col s8">
-                            {{ $company->name }}
-                        </div>
-                        <div class="col s4 right-align">
-                            <a href="{{ route('company_details', ['company_id' => $company->id]) }}" class="btn"><i class="fas fa-book-open"></i></a>
-                            <a href="{{ route('confirm_company', ['company_id' => $company->id]) }}" class="btn"><i class="fas fa-check"></i></a>
-                            <a href="{{ route('refuse_company', ['company_id' => $company->id]) }}" class="btn"><i class="fas fa-ban"></i></a>
-                        </div>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
-      </div>
-    </li>
-    <li>
-      <div class="collapsible-header">
-        Activités en attente ({{ $nombre_activities_attente }})
-      </div>
-      <div class="collapsible-body">
-        <h5 class="pb-2">Activités en attente</h5>
-        <ul>
-            @if (count($activities) >= 1)
-            @foreach($activities as $activity)
-                <li>
-                    <div class="row z-depth-1 pt-1 px-1 pb-1">
-                        <div class="col s8">
-                            {{ $activity->name }}
-                        </div>
-                        <div class="col s4 right-align">
-                            <a href="{{ route('activity_details', ['activity_id' => $activity->id]) }}" class="btn"><i class="fas fa-book-open"></i></a>
-                            <a href="{{ route('confirm_activity', ['activity_id' => $activity->id]) }}" class="btn"><i class="fas fa-check"></i></a>
-                            <a href="{{ route('refuse_activity', ['activity_id' => $activity->id]) }}" class="btn"><i class="fas fa-ban"></i></a>
-                        </div>
-                    </div>
-                </li>
-            @endforeach
-            @else
-              Aucune activité à valider
-            @endif
-        </ul>
-      </div>
-    </li>
-    <li>
-      <div class="collapsible-header">
-        Commentaires en attente ({{ $nombre_comments_attente }})
-      </div>
-      <div class="collapsible-body">
-        <h5 class="pb-2">Commentaires en attente</h5>
-        <ul>
-            @if (count($comments) >= 1)
-            @foreach($comments as $comment)
-                <li>
-                    <div class="row z-depth-1 pt-1 px-1 pb-1">
-                        <div class="col s8">
-                            {{ $comment->title }} : {{ \Illuminate\Support\Str::limit($comment->message, $limit = 75, $end = '...') }}
-                        </div>
-                        <div class="col s4 right-align">
-                            <a href="#" class="btn" style="margin: 1px"><i class="fas fa-book-open"></i></a>
-                            <a href="#" class="btn" style="margin: 1px"><i class="fas fa-check"></i></a>
-                            <a href="#" class="btn" style="margin: 1px"><i class="fas fa-ban"></i></a>
-                        </div>
-                    </div>
-                </li>
-            @endforeach
-            @else
-              Aucun commentaire à valider !
-            @endif
-        </ul>
-      </div>
-    </li>
-  </ul>
+	<ul class="collapsible accordeon">
+		<li>
+			<div class="collapsible-header">
+				Structures en attente ({{ $nombre_companies_attente }})
+			</div>
+			<div class="collapsible-body">
+				<h5 class="pb-2">Structures en attente</h5>
+				<ul>
+					@foreach($companies as $company)
+						<li>
+							<div class="row z-depth-1 pt-1 px-1 pb-1">
+								<div class="col m8 s12">
+									{{ $company->name }}
+								</div>
+								<div class="col m4 s12 right-align">
+									<a href="{{ route('company_details', ['company_id' => $company->id]) }}" class="btn"><i class="fas fa-book-open"></i></a>
+									<a href="{{ route('confirm_company', ['company_id' => $company->id]) }}" class="btn"><i class="fas fa-check"></i></a>
+									<a href="{{ route('refuse_company', ['company_id' => $company->id]) }}" class="btn"><i class="fas fa-ban"></i></a>
+								</div>
+							</div>
+						</li>
+					@endforeach
+				</ul>
+			</div>
+		</li>
+		<li>
+			<div class="collapsible-header">
+				Activités en attente ({{ $nombre_activities_attente }})
+			</div>
+			<div class="collapsible-body">
+				<h5 class="pb-2">Activités en attente</h5>
+				<ul>
+					@if (count($activities) >= 1)
+					@foreach($activities as $activity)
+						<li>
+							<div class="row z-depth-1 pt-1 px-1 pb-1">
+								<div class="col m8 s12">
+									{{ $activity->name }}
+								</div>
+								<div class="colm4 s12 right-align">
+									<a href="{{ route('activity_details', ['activity_id' => $activity->id]) }}" class="btn"><i class="fas fa-book-open"></i></a>
+									<a href="{{ route('confirm_activity', ['activity_id' => $activity->id]) }}" class="btn"><i class="fas fa-check"></i></a>
+									<a href="{{ route('refuse_activity', ['activity_id' => $activity->id]) }}" class="btn"><i class="fas fa-ban"></i></a>
+								</div>
+							</div>
+						</li>
+					@endforeach
+					@else
+						Aucune activité à valider
+					@endif
+				</ul>
+			</div>
+		</li>
+		<li>
+			<div class="collapsible-header">
+				Commentaires en attente ({{ $nombre_comments_attente }})
+			</div>
+			<div class="collapsible-body">
+				<h5 class="pb-2">Commentaires en attente</h5>
+				<ul>
+					@if (count($comments) >= 1)
+					@foreach($comments as $comment)
+						<li>
+							<div class="row z-depth-1 pt-1 px-1 pb-1">
+								<div class="col m8 s12">
+									{{ $comment->title }} : {{ \Illuminate\Support\Str::limit($comment->message, $limit = 75, $end = '...') }}
+								</div>
+								<div class="colm4 s12 right-align">
+									<a href="#" class="btn" style="margin: 1px"><i class="fas fa-book-open"></i></a>
+									<a href="#" class="btn" style="margin: 1px"><i class="fas fa-check"></i></a>
+									<a href="#" class="btn" style="margin: 1px"><i class="fas fa-ban"></i></a>
+								</div>
+							</div>
+						</li>
+					@endforeach
+					@else
+					Aucun commentaire à valider !
+					@endif
+				</ul>
+			</div>
+		</li>
+	</ul>
 
   <div class="row">
     <div class="col s12 z-depth-1 px-1">
