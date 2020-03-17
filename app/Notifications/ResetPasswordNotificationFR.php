69a1fe36b0f0b7e9f005ef5b4202c2d6d7370bdf
Nyway <48278317+Nywayz@users.noreply.github.com>
2020-03-17 16:22:32 +0100

    Reset 5

--- app/Notifications/ResetPasswordNotificationFR.php
+++ app/Notifications/ResetPasswordNotificationFR.php
@@ -58,11 +58,11 @@ class ResetPasswordNotificationFR extends Notification
         }
 
         return (new MailMessage)
-            ->subject(Lang::getFromJson('Reset Password Notification'))
-            ->line(Lang::getFromJson('Yozedzekfzefl because we received a password reset request for your account.'))
-            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
-            ->line(Lang::getFromJson('This passwzezepire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
+            ->subject('Reset Password Notification'))
+            ->line('Yozedzekfzefl because we received a password reset request for your account.')
+            ->action('Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false))
+            ->line('This passwzezepire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')])
+            ->line('If you did not request a password reset, no further action is required.'))
     }
 
     /**
