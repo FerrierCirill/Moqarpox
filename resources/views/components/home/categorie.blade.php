dc1cf2a486e5e36df5ed75ca55e1163f3652f193
cirill <cirill@asheart.fr>
2020-03-17 16:20:36 +0100

    category homme card

--- resources/views/components/home/categorie.blade.php
+++ resources/views/components/home/categorie.blade.php
@@ -1,8 +1,8 @@
-<div class="row pt-5" id="scrollToCategorie">
+<div class="row pt-5 mb-4" id="scrollToCategorie">
     @forelse($categories as $category)
         <div class="col s12 m6 l3">
              <div class="card microSmallCard hoverable">
-                <div class="card-image" style="overflow: hidden;height: 185px;">
+                <div class="card-image" style="overflow: hidden;height: 235px;">
                     <img class="card-category" src="{{ asset($category->link) }}">
                     <span class="card-title">{{$category->name}}</span>
                     {{-- <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a> --}}
@@ -10,6 +10,6 @@
             </div>
         </div>
     @empty
-        
+
     @endforelse
 </div>
