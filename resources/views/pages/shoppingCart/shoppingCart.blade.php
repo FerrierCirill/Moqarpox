0c08990abc3b9ea33b81baa28f5028c8751683c2
cirill <cirill@asheart.fr>
2020-03-18 16:24:46 +0100

    panier piano

--- resources/views/pages/shoppingCart/shoppingCart.blade.php
+++ resources/views/pages/shoppingCart/shoppingCart.blade.php
@@ -82,7 +82,7 @@
             <div class="h-full col l3 m12 shoppingCart-rightZone pt-2 px-2">
                 @if(\Auth::check())
                     <h5 class="white-text mb-2">Total : {{$total}} €</h5>
-                    <button class="btn w-100" type="submit">Valider votre pannier</button>
+                    <button class="btn w-100" type="submit">Valider votre panier</button>
 
                     <a href="{{route('shopping_cart_validate')}}">
                         <p>Vous avez deja remplie toute ces informations ? Reprenez votre panier ou vous l'avez laisser ici</p>
