91ade1bf0777b741f5aa254bf6349ca69aa7fc0a
BlackFox3000 <flo-ti@hotmail.fr>
2020-03-24 14:08:46 +0100

    Nettoyage bdd

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,28 +23,28 @@ class UsersTableSeeder extends Seeder
             'admin' => 1
         ]);
 
-        DB::table('users')->insert([
-            'first_name' => 'client',
-            'second_name' => 'client',
-            'phone' => '0000000000',
-            'email' => 'client@client.com',
-            'password' => bcrypt('client'),
-            'state' => 0,
-            'civility' => 'man',
-            'admin' => 0
-        ]);
+//        DB::table('users')->insert([
+//            'first_name' => 'client',
+//            'second_name' => 'client',
+//            'phone' => '0000000000',
+//            'email' => 'client@client.com',
+//            'password' => bcrypt('client'),
+//            'state' => 0,
+//            'civility' => 'man',
+//            'admin' => 0
+//        ]);
+//
+//        DB::table('users')->insert([
+//            'first_name' => 'provider',
+//            'second_name' => 'provider',
+//            'phone' => '0000000000',
+//            'email' => 'provider@provider.com',
+//            'password' => bcrypt('provider'),
+//            'state' => 1,
+//            'civility' => 'man',
+//            'admin' => 0
+//        ]);
 
-        DB::table('users')->insert([
-            'first_name' => 'provider',
-            'second_name' => 'provider',
-            'phone' => '0000000000',
-            'email' => 'provider@provider.com',
-            'password' => bcrypt('provider'),
-            'state' => 1,
-            'civility' => 'man',
-            'admin' => 0
-        ]);
-
-        factory(\App\User::class, 1000)->create();
+     //   factory(\App\User::class, 1000)->create();
     }
 }
